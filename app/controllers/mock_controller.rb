c8f39ab71fb32a689de55f63216f4800f89b1d67
baldino <baldino@baldino-pc.(none)>
2012-05-04 18:54:29 +0400

    rm some files

--- app/controllers/mock_controller.rb
+++
@@ -1,7 +0,0 @@
-class MockController < ApplicationController
-  def blocks
-    data = Block.where(:page_id => params[:page_id])
-    response.headers['Content-type'] = "text/plain; charset=utf-8"
-    render :text => data.to_json
-  end
-end
